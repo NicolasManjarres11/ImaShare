f1fd693645e5558e0857b284af4e23b6ef3a0d6a
Nicolas Manjarres <nicolasmanjarresg@gmail.com>
2024-02-21 20:37:13 -0500

    se agrega nuevo cluster de base de datos

--- src/keys.js
+++ src/keys.js
@@ -1,7 +1,8 @@
 module.exports = {
     database: {
         URI2: 'mongodb://localhost/imashare',
-        URI: 'mongodb+srv://nicolasadmin:soporte@imashare.yn3pkbz.mongodb.net/imashare'
+        URI3: 'mongodb+srv://nicolasadmin:soporte@imashare.yn3pkbz.mongodb.net/imashare',
+        URI: 'mongodb+srv://nicolasadmin:soporte@imashare.iw5qfja.mongodb.net/imashare'
     }
 }
 //
